cd826eb71868ebae5482d888675f5c14e5c524f2
Oriery <panev291002@gmail.com>
2023-12-10 23:39:53 +0300

    read lines from serial port

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
+
+SerialPort.list().then((ports) => {
+  ports.forEach((port) => {
+    console.log(port);
+  });
+});
+
+// Create a port
+const port = new SerialPort({
+  path: "COM5",
+  baudRate: 115200,
+});
+
+const parser = new ReadlineParser()
+port.pipe(parser)
+
+port.on('error', function(err) {
+  //console.error('Error: ', err.message)
+  throw err
+})
+
+port.on('open', function() {
+  write('Hi, Serial port')
+})
+
+parser.on('data', function (data) {
+  console.log(data)
+})
+
+function write(data : string) {
+  port.write(data, function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+    console.log('message written')
+  })
+}
